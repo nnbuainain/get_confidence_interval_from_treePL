ce85d8ee6a44d42448fe59134a2652db2b046910
Maura Regina <mauraregina@gmail.com>
2022-01-13 06:50:54 -0400

    add method in order to extract trees
    Signed-off-by: Maura Regina <mauraregina@gmail.com>

--- split_trees_exabayes_py/split_trees_exabayes.py
+++ split_trees_exabayes_py/split_trees_exabayes.py
@@ -4,15 +4,23 @@ def get_legend(content: str) -> dict:
     legend_str = content.split('translate')[1].split(';')[0]
     legend_str = legend_str.replace('\n', '').lstrip()
     legend_dict = dict(x.split("	") for x in legend_str.split(",	"))
-
     return legend_dict
+
+def get_tree(content: str):
+    tree_str = content[content.index('tree gen.0.{0}'):].split('end')[0]
+    return tree_str
 
 def main():
     content = util.read_file()
-    legend_dict = get_legend(content)
 
-    for k, v in legend_dict.items():
-        print(k, v)
+    print('Extracting legend...')
+    legend_dict = get_legend(content)
+    print(f'Size of the legend: {len(legend_dict)}')
+
+    print('Extracting trees...')
+    tree_str = get_tree(content)
+    print(f'First two trees...{tree_str[:13522]}')
+    print(f'Last two trees... {tree_str[18036750:]}')
 
 if __name__ == "__main__":
     main()
