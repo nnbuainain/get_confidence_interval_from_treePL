f76d07f690d0fbaf2abbfd7e5fac05ca52a49af5
Maura Regina <mauraregina@gmail.com>
2022-01-13 21:40:06 -0400

    convert tree_str into a list and select last 100 trees
    Signed-off-by: Maura Regina <mauraregina@gmail.com>

--- split_trees_exabayes_py/split_trees_exabayes.py
+++ split_trees_exabayes_py/split_trees_exabayes.py
@@ -6,21 +6,26 @@ def get_legend(content: str) -> dict:
     legend_dict = dict(x.split("	") for x in legend_str.split(",	"))
     return legend_dict
 
-def get_tree(content: str):
-    tree_str = content[content.index('tree gen.0.{0}'):].split('end')[0]
-    return tree_str
+def get_tree(content: str) -> list:
+    content = content.replace('\n', '').lstrip()
+    tree_str = content[content.index('tree gen.0.{0}'):].split('end;')[0]
+    tree_list = tree_str.split(';')[-101:-1]
+    return tree_list
 
 def main():
     content = util.read_file()
 
-    print('Extracting legend...')
+    # Extracting legend from file into a dictionary
     legend_dict = get_legend(content)
-    print(f'Size of the legend: {len(legend_dict)}')
+    print('\n--------------------- LEGEND ---------------------')
+    for key, value in legend_dict.items():
+        print(key, ' : ', value)
 
-    print('Extracting trees...')
-    tree_str = get_tree(content)
-    print(f'First two trees...{tree_str[:13522]}')
-    print(f'Last two trees... {tree_str[18036750:]}')
+    # Extracting last 100 trees from file into a list
+    tree_list = get_tree(content)
+    print('\n--------------------- TREES ---------------------')
+    for index, tree in enumerate(tree_list, start=1):
+        print(f'{index}{tree}')
 
 if __name__ == "__main__":
     main()
--- split_trees_exabayes_py/utils/util.py
+++ split_trees_exabayes_py/utils/util.py
@@ -1,8 +1,6 @@
 import constant
 
 def read_file():
-    print('\nReading file...')
-
     with open(constant.FILE) as f:
         content = f.read()
 
