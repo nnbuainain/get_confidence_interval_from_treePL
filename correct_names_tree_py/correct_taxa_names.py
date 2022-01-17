6781f6388ae4b27dec1db4964c337ea60a25391a
Nelson Buainain <nnbuainain@gmail.com>
2022-01-17 14:08:45 -0400

    cleaned coorec_taxa_names.py file
    Signed-off-by: Nelson Buainain <nnbuainain@gmail.com>

--- correct_names_tree_py/correct_taxa_names.py
+++ correct_names_tree_py/correct_taxa_names.py
@@ -1,12 +1,13 @@
 import pandas as pd
 import glob
 
-# import dictionary as spreadsheets
+# Import dictionary as spreadsheets
 dict = pd.read_excel('./dict_correct_names.xlsx')
 
-# format dictionary according to python
+# Format dictionary according to python
 dict = dict.set_index('nome_original')['nome_final'].to_dict()
 
+# Iter over files applying the rename function with the dictionary with original and final names
 for index, file in enumerate(glob.iglob('../exabayes_phylo_trees/original/exabayes*')):
     with open(file, 'r') as f:
         content = f.read()
@@ -15,11 +16,3 @@ for index, file in enumerate(glob.iglob('../exabayes_phylo_trees/original/exabay
 
     with open(f'../exabayes_phylo_trees/exabayes_trees_run_{index+1}','w+') as f:
         f.write(content)
-
-# with open(f'../exabayes_phylo_trees/original/exabayes_trees_run_{1}', 'r') as f:
-#     content = f.read()
-#     for original, final in dict.items():
-#         content = content.replace(original, final)
-#
-# with open(f'../exabayes_phylo_trees/exabayes_trees_run_{1}', 'w+') as f:
-#     f.write(content)
